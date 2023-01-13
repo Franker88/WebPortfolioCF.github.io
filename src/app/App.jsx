28367422a7cff027aa5fda5512bb912b3c386b2b
Franker88 <franker5588@gmail.com>
2023-01-13 18:59:07 -0400

    ADD Hero content V1

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import "./App.css";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "../components/layout/Layout";
 import { Home } from "../routes/home/Home";
 import { NotFound } from "../routes/notfound/NotFound";
+import { Diplomas } from "../routes/diplomas/Diplomas";
+import { Projects } from "../routes/projects/Projects";
 
 function App() {
   return (
     <React.Fragment>
-      <HashRouter>
+      <BrowserRouter>
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/diplomas" element={<Diplomas />} />
+            <Route path="/projects" element={<Projects />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
-      </HashRouter>
+      </BrowserRouter>
     </React.Fragment>
   );
 }
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,88 @@
 import React from "react";
+import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faMailBulk } from "@fortawesome/free-solid-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faCopyright } from "@fortawesome/free-regular-svg-icons";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>All Right Reserved</p>
-      <p>Made with React</p>
+    <footer className="Footer">
+      <section className="Contact-section">
+        <div className="Contact-item">
+          <h4 className="Footer-title">
+            <FontAwesomeIcon icon={faInstagram} /> Instagram
+          </h4>
+          <p className="Footer-info Ig">
+            <a
+              href="https://www.instagram.com/franco.c.v/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="Footer-anchor"
+            >
+              @franco.c.v
+            </a>
+          </p>
+        </div>
+        <div className="Contact-item">
+          <h4 className="Footer-title">
+            <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+          </h4>
+          <p className="Footer-info In">
+            <a
+              href="https://www.linkedin.com/in/carlos-alejandro-franco-vel%C3%A1squez-670421145/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="Footer-anchor"
+            >
+              {" "}
+              Carlos Franco
+            </a>
+          </p>
+        </div>
+        <div className="Contact-item">
+          <h4 className="Footer-title">
+            <FontAwesomeIcon icon={faGithub} /> Git Hub
+          </h4>
+          <p className="Footer-info Gh">
+            <a
+              href="https://github.com/Franker88"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="Footer-anchor"
+            >
+              Franker88
+            </a>
+          </p>
+        </div>
+        <div className="Contact-item">
+          <h4 className="Footer-title">
+            <FontAwesomeIcon icon={faMailBulk} /> Gmail
+          </h4>
+          <p className="Footer-info Ig">franker5588@gmail.com</p>
+        </div>
+        <div className="Contact-item">
+          <h4 className="Footer-title">
+            <FontAwesomeIcon icon={faMailBulk} /> Alternative Gm
+          </h4>
+          <p className="Footer-info In">cafrancov2510@gmail.com</p>
+        </div>
+        <div className="Contact-item">
+          <h4 className="Footer-title">
+            <FontAwesomeIcon icon={faPhone} /> Phone
+          </h4>
+          <p className="Footer-info Gh">+58 414-9008364</p>
+        </div>
+      </section>
+      <section className="Rights">
+        <p>Made with React.js</p>
+        <p>
+          All Rights Reserved <FontAwesomeIcon icon={faCopyright} />
+        </p>
+      </section>
     </footer>
   );
 };
